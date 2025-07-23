e914bcb2609cf7aa62d8a6eceb1d157c967243f2
Prachurya Ray <prachuryaray@gmail.com>
2025-07-23 11:00:38 +0530

    exploring GET,POST,DELETE,PATCH

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,35 @@ const express = require("express");
 
 const app = express();
 
+
+//handling GET request
+app.get("/user",(req,res)=>{
+  res.send({firstName:"Prachurya", lastName:"Ray"});
+});
+
+app.post("/user", (req, res) => {
+  res.send("Saved");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Saved");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted");
+});
+
+
 app.use("/test", (req, res) => {
   res.send("Hello to our test");
 });
 app.use("/check", (req, res) => {
   res.send("Hello for checking");
 });
-app.use((req, res) => {
+app.use("/", (req, res) => {
   res.send("Hello from our server!");
 });
 
-app.listen(3000, ()=>{
-    console.log('Successfully connected to 3000');
+app.listen(3000, () => {
+  console.log("Successfully connected to 3000");
 });
