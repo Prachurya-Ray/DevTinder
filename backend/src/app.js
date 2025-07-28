b4026289364ceb77783b47db78059f53fa052ffd
Prachurya Ray <prachuryaray@gmail.com>
2025-07-28 14:32:26 +0530

    playing with route handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,19 +3,17 @@ const express = require("express");
 const app = express();
 
 
-//handling GET request
-app.get(/.*fly$/,(req,res)=>{
-  res.send({firstName:"Prachurya", lastName:"Ray"});
-});
+app.use('/user',[(req,res,next)=>{
+  console.log('hii')
+  next();
 
-//handling GET request
-app.get("/user/:userID/:name/:pw",(req,res)=>{
-  console.log(req.params)
-  console.log(req.query)
-  res.send({firstName:"Prachurya", lastName:"Ray"});
-});
+  // res.send('Hello')
+},(req,res,next)=>{
+  console.log('hii 2')
+  res.send('Hello 2')
+  // next();
 
-
+}])
 
 app.listen(3000, () => {
   console.log("Successfully connected to 3000");
