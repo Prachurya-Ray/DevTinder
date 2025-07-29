df6c5d918a61bc1b3e239425a186880365358a52
Prachurya Ray <prachuryaray@gmail.com>
2025-07-29 23:45:50 +0530

    user post api created

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const user = require("./models/user");
+
 const app = express();
+
+app.post("/user", async (req, res) => {
+  const userObj = {
+    firstName: "Prachurya",
+    lastName: "Ray",
+    email: "prachuryaray@gmail.com",
+    password: "Tinder123",
+    age: 25,
+    gender: "Male",
+  };
+  const User = new user(userObj);
+
+  try {
+    await User.save();
+    res.send("Saved");
+  } catch (err) {
+    res.status(400).send("Can't save" + err);
+  }
+});
 
 connectDB()
   .then(() => {
@@ -12,5 +33,3 @@ connectDB()
   .catch((err) => {
     console.error(err);
   });
-
-
