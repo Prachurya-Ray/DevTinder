058921922d390cbb7e5e85248a8878ec7d44bbb5
Prachurya Ray <prachuryaray@gmail.com>
2025-07-30 23:59:22 +0530

    trying dynamic data send

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,22 +5,22 @@ const user = require("./models/user");
 const app = express();
 
 app.post("/user", async (req, res) => {
-  const userObj = {
-    firstName: "Prachurya",
-    lastName: "Ray",
-    email: "prachuryaray@gmail.com",
-    password: "Tinder123",
-    age: 25,
-    gender: "Male",
-  };
-  const User = new user(userObj);
+  // const userObj = {
+  //   firstName: "Prachurya",
+  //   lastName: "Ray",
+  //   email: "prachuryaray@gmail.com",
+  //   password: "Tinder123",
+  //   age: 25,
+  //   gender: "Male",
+  // };
+  // const User = new user(userObj);
 
-  try {
-    await User.save();
-    res.send("Saved");
-  } catch (err) {
-    res.status(400).send("Can't save" + err);
-  }
+  // try {
+  //   await User.save();
+  //   res.send("Saved");
+  // } catch (err) {
+  //   res.status(400).send("Can't save" + err);
+  // }
 });
 
 connectDB()
