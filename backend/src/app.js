5f5a4c9f7ae4c12d0844a4de92379105817920e4
Prachurya Ray <prachuryaray@gmail.com>
2025-07-31 21:52:42 +0530

    get request to get the user data

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,46 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const user = require("./models/user");
+const User = require("./models/user");
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/user", async (req, res) => {
-
   // dynamic signup api to receive data from end user
-  const User = new user(req.body);
-
+  const user = new User(req.body);
   try {
-    await User.save();
+    await user.save();
     res.send("Saved");
   } catch (err) {
     res.status(400).send("Can't save" + err);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userMail = req.body.email;
+  const userData = await User.find({ email: userMail });
+  try {
+    if (userData.length==0) {
+      res.status(400).send("Data not found");
+    } else {
+      res.send(userData);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const userData = await User.find({});
+  try {
+    if (userData.length == 0) {
+      res.status(400).send("Data not found");
+    } else {
+      res.send(userData);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
   }
 });
 
